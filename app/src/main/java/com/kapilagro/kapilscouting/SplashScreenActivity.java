f070be0ca2e96db0ec918ccdc874b12c60605021
swamy3697 <swamyp2002@gmail.com>
2025-03-17 15:30:13 +0530

    first commit

---
+++ app/src/main/java/com/kapilagro/kapilscouting/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package com.kapilagro.kapilscouting;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    // Splash screen display time in milliseconds
+    private static final int SPLASH_DISPLAY_TIME = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Handler to start the MainActivity after the splash screen duration
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Create an Intent to start MainActivity
+                Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish(); // Close the splash screen activity
+            }
+        }, SPLASH_DISPLAY_TIME);
+    }
+}
